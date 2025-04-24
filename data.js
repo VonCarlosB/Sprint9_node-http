36a0373b9e6250a22361f49a4ecae90036da09e4
CarlosBF <cborrellf@outlook.es>
2025-04-24 19:42:09 +0200

    Corrected some typos

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ function importantData(){
     let data = {
         title:'Alquimia básica',
         subtitle:'La cocina en la fantasía',
-        description:'En un mundo de fantasía con mágia, brujería, criaturas ancestrales y monstruos inimaginables, la cocina debe ser un refugio no solo del cuerpo sino también del alma. ¿Cómo integrar el arte de la cocina en un mundo oscuro y perturvador como el descrito en la alta fantasía'
+        description:'En un mundo de fantasía con mágia, brujería, criaturas ancestrales y monstruos inimaginables, la cocina debe ser un refugio no solo del cuerpo sino también del alma. ¿Cómo integrar el arte de la cocina en un mundo oscuro y perturvador como el descrito en la alta fantasía?'
     }
     return data
 }
